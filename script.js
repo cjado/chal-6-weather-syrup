da6684743978d779f324185dd28fe7d155d158f2
Cj adorna <cjadorna@hotmail.com>
2023-01-18 20:08:28 -0600

    fetch, first infomration bos completed, list appeneded to reflet to start cities

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 $( function() {
     var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
+      "Houston",
+      "Austin"
     ];
     $( "#search-input" ).autocomplete({
       source: availableTags
     });
   } );
+
+
+var APIkey = "43a3b99ad80001e205832282fce85783";
+var city;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey + "&units=imperial";
+
+$(document).ready(function(){
+  $('#searchBtn').click(function(){
+    var city = $('#search-input').val();
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey + "&units=imperial";
+    console.log(queryURL)
+    fetch(queryURL)
+    .then(reponse => reponse.json())
+    .then(data => {
+      $('#city').text(data.name );
+      $('#temp').text(data.main.temp) + "°F";
+      $('#wind').text(data.wind.speed + " mph");
+      $('#humidity').text(data.main.humidity + "%");
+    }).catch(error => {
+      console.log(error);
+    });
+  });
+});
+
